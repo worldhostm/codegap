ecf5dd1e508e9588f7fb09264d4fc85f598a9520
chos1909@gmail.com <chos1909@gmail.com>
2024-12-10 17:10:54 +0900

    adsads

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -10,55 +10,92 @@ export default function page() {
     animation: "rotateThenReverse 4s linear infinite",
   };
 
-
-  const [trafficData, settrafficData] = useState({});
-  useEffect(() => {
-    const canvas = document.getElementById('lineGraph') as HTMLCanvasElement;
-    const ctx = canvas?.getContext('2d'); // CanvasRenderingContext2D 가져오기
-      // 그라데이션 설정
-    const gradient = ctx?.createLinearGradient(500, 0, 100, 0);
-    console.info(gradient);
-    gradient?.addColorStop(0, 'rgba(75, 192, 192, 1)'); // 상단 색상
-    // gradient?.addColorStop(1, 'rgba(75, 192, 192, 0)');   // 하단 색상 (투명)
-    const trafficData1 = {
-      labels: ['Organic Search', 'Direct', 'Referral', 'Social', 'Paid Search'],
-      datasets: [
-        {
-          label: 'Traffic Sources',
-          data: [80, 30, 15, 10, 5], // 트래픽 소스의 비율
-          // backgroundColor: [
-          //   'rgba(75, 192, 192, 0.2)',
-          //   'rgba(255, 99, 132, 0.2)',
-          //   'rgba(255, 205, 86, 0.2)',
-          //   'rgba(54, 162, 235, 0.2)',
-          //   'rgba(153, 102, 255, 0.2)',
-          // ],
-          backgroundColor: 'red',
-          fill:'true',
-          borderColor: [
-            'rgba(75, 192, 192, 1)',
-            'rgba(255, 99, 132, 1)',
-            'rgba(255, 205, 86, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(153, 102, 255, 1)',
-          ],
-          borderWidth: 3,
-          options : {
-            scales: {
-              x: {
-                type: 'category', // 카테고리 스케일
-              },
-              y: {
-                type: 'linear', // linear 스케일
-                beginAtZero: true,
-              },
+  const trafficData = {
+    labels: ['Organic Search', 'Direct', 'Referral', 'Social', 'Paid Search'],
+    datasets: [
+      {
+        label: 'Traffic Sources',
+        data: [80, 30, 15, 10, 5], // 트래픽 소스의 비율
+        // backgroundColor: [
+        //   'rgba(75, 192, 192, 0.2)',
+        //   'rgba(255, 99, 132, 0.2)',
+        //   'rgba(255, 205, 86, 0.2)',
+        //   'rgba(54, 162, 235, 0.2)',
+        //   'rgba(153, 102, 255, 0.2)',
+        // ],
+        backgroundColor: 'red',
+        fill:'true',
+        borderColor: [
+          'rgba(75, 192, 192, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(255, 205, 86, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(153, 102, 255, 1)',
+        ],
+        borderWidth: 3,
+        options : {
+          scales: {
+            x: {
+              type: 'category', // 카테고리 스케일
             },
-          }
-        },
-      ]
-    };
-    settrafficData(trafficData1);
-  }, [trafficData])
+            y: {
+              type: 'linear', // linear 스케일
+              beginAtZero: true,
+            },
+          },
+        }
+      },
+    ]
+  };
+
+  // const [trafficData, settrafficData] = useState({});
+  // useEffect(() => {
+  //   const canvas = document.getElementById('lineGraph') as HTMLCanvasElement;
+  //   const ctx = canvas?.getContext('2d'); // CanvasRenderingContext2D 가져오기
+  //     // 그라데이션 설정
+  //   const gradient = ctx?.createLinearGradient(500, 0, 100, 0);
+  //   console.info(gradient);
+  //   gradient?.addColorStop(0, 'rgba(75, 192, 192, 1)'); // 상단 색상
+  //   // gradient?.addColorStop(1, 'rgba(75, 192, 192, 0)');   // 하단 색상 (투명)
+  //   const trafficData1 = {
+  //     labels: ['Organic Search', 'Direct', 'Referral', 'Social', 'Paid Search'],
+  //     datasets: [
+  //       {
+  //         label: 'Traffic Sources',
+  //         data: [80, 30, 15, 10, 5], // 트래픽 소스의 비율
+  //         // backgroundColor: [
+  //         //   'rgba(75, 192, 192, 0.2)',
+  //         //   'rgba(255, 99, 132, 0.2)',
+  //         //   'rgba(255, 205, 86, 0.2)',
+  //         //   'rgba(54, 162, 235, 0.2)',
+  //         //   'rgba(153, 102, 255, 0.2)',
+  //         // ],
+  //         backgroundColor: 'red',
+  //         fill:'true',
+  //         borderColor: [
+  //           'rgba(75, 192, 192, 1)',
+  //           'rgba(255, 99, 132, 1)',
+  //           'rgba(255, 205, 86, 1)',
+  //           'rgba(54, 162, 235, 1)',
+  //           'rgba(153, 102, 255, 1)',
+  //         ],
+  //         borderWidth: 3,
+  //         options : {
+  //           scales: {
+  //             x: {
+  //               type: 'category', // 카테고리 스케일
+  //             },
+  //             y: {
+  //               type: 'linear', // linear 스케일
+  //               beginAtZero: true,
+  //             },
+  //           },
+  //         }
+  //       },
+  //     ]
+  //   };
+  //   settrafficData(trafficData1);
+  // }, [trafficData])
   
 
   return (
